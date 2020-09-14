669b7f1829799e523ac58d7f053a4119b40fad7a
Lucas <lucas.odonnelll@gmail.com>
2020-09-14 14:35:14 -0700

    Made queen valid moves

--- chess/queen.rb
+++ chess/queen.rb
@@ -1,4 +1,4 @@
-require_relative "queen"
+require_relative "piece"
 class Queen 
     attr_reader :name, :color
     attr_accessor :pos, :count
@@ -8,4 +8,85 @@ class Queen
         @color = color 
         @name = @color == "white" ? "\u265B" : "\u2655"
     end
+
+    def valid_move?(new_pos)
+      pos = self.pos
+      if up_right(pos).include?(new_pos) || up_left(pos).include?(new_pos)
+        true
+      elsif down_left(pos).include?(new_pos) || down_right(pos).include?(new_pos)
+        true
+      elsif up(pos).include?(new_pos) || right(pos).include?(new_pos)
+        true
+      elsif left(pos).include?(new_pos) || down(pos).include?(new_pos)
+        true
+      else
+        false
+      end
+    end
+
+    def up_right(pos)
+      array = [pos] 
+
+      until array.last[0] == 7 || array.last[1] == 7
+        array << [array.last[0] + 1, array.last[1] + 1]
+      end
+      array 
+    end
+
+    def up_left(pos)
+      array = [pos]
+      until array.last[0] == 7 || array.last[1] == 0
+        array << [array.last[0] + 1, array.last[1] - 1]
+      end
+      array
+    end
+
+    def down_right(pos)
+      array = [pos]
+      until array.last[0] == 0 || array.last[1] == 7
+        array << [array.last[0] - 1, array.last[1] + 1]
+      end
+      array
+    end
+
+    def down_left(pos)
+      array = [pos]
+      until array.last[0] == 0 || array.last[1] == 0
+        array << [array.last[0] - 1, array.last[1] - 1]
+      end
+      array
+    end
+      
+    def up(pos)
+      array = [pos] 
+      until array.last[0] == 0 
+        array << [array.last[0] - 1, array.last[1]]
+      end
+      array 
+    end
+
+    def left(pos)
+      array = [pos]
+      until array.last[1] == 0
+        array << [array.last[0], array.last[1] - 1]
+      end
+      array
+    end
+
+    def right(pos)
+      array = [pos]
+      until array.last[1] == 7
+        array << [array.last[0], array.last[1] + 1]
+      end
+      array
+    end
+
+    def down(pos)
+      array = [pos]
+      until array.last[0] == 7
+        array << [array.last[0] + 1, array.last[1]]
+      end
+      array
+    end
 end
+
